1548d3b8972556e46cb846e6613219236d9c2612
mmmpipi <2519058820@qq.com>
2025-11-04 11:25:29 +0800

    卢浮宫再重构

--- src/memes/louvre.rs
+++ src/memes/louvre.rs
@@ -1,4 +1,6 @@
-use skia_safe::{Color, ColorFilter, ColorMatrix, HSV, IPoint, Image, TileMode, image_filters};
+use skia_safe::{
+    BlendMode, Color, ColorMatrix, IPoint, IRect, ISize, Image, ImageFilter, Paint, Point, Shader, TileMode, gradient_shader::{self, GradientShaderColors}, image_filters::{self, CropRect}, scalar
+};
 
 use meme_generator_core::error::Error;
 use meme_generator_utils::{
@@ -10,117 +12,218 @@ use meme_generator_utils::{
 
 use crate::{options::GrayStandard, register_meme};
 
-fn louvre(images: Vec<InputImage>, _: Vec<String>, options: GrayStandard) -> Result<Vec<u8>, Error> {
-    let gray_standard = options.gray_standard.unwrap_or_else(|| {"rec601_gray".to_string()});
+fn create_convolute_average(width: usize) -> Vec<f32> {
+    let value = 1.0 / (width * width) as f32;
+    vec![value; width * width]
+}
+
+fn simple_convolute_average_builder(width: usize) -> MatrixConvolutionBuilder {
+    let width_int = width as i32;
+    let mut builder = MatrixConvolutionBuilder::new(
+        (width_int, width_int),
+        create_convolute_average(width),
+        ((width_int - 1) / 2, (width_int - 1) / 2),
+    );
+    builder.convolve_alpha = false;
+    builder
+}
+
+#[derive(Clone)]
+struct MatrixConvolutionBuilder {
+    pub kernel_size: ISize,
+    pub kernel: Vec<scalar>,
+    pub gain: scalar,
+    pub bias: scalar,
+    pub kernel_offset: IPoint,
+    pub tile_mode: TileMode,
+    pub convolve_alpha: bool,
+    pub input: Option<ImageFilter>,
+    pub crop_rect: CropRect,
+}
+
+impl MatrixConvolutionBuilder {
+    fn new(
+        kernel_size: impl Into<ISize>,
+        kernel: impl Into<Vec<scalar>>,
+        kernel_offset: impl Into<IPoint>,
+    ) -> Self {
+        Self {
+            kernel_size: kernel_size.into(),
+            kernel: kernel.into(),
+            gain: 1.0,
+            bias: 0.0,
+            kernel_offset: kernel_offset.into(),
+            tile_mode: TileMode::Clamp,
+            convolve_alpha: true,
+            input: None,
+            crop_rect: None.into(),
+        }
+    }
+    fn build(self) -> Option<ImageFilter> {
+        image_filters::matrix_convolution(
+            self.kernel_size,
+            &self.kernel.to_vec(),
+            self.gain,
+            self.bias,
+            self.kernel_offset,
+            self.tile_mode,
+            self.convolve_alpha, // convolve_alpha，是否对alpha通道进行卷积
+            self.input,
+            self.crop_rect,
+        )
+    }
+}
+
+fn create_kiss_gradient(isize: impl Into<ISize>) -> Option<Shader> {
+    let colors = [
+        Color::from_rgb(251, 186, 48), // #fbba30
+        Color::from_rgb(252, 114, 53), // #fc7235
+        Color::from_rgb(252, 53, 78),  // #fc354e
+        Color::from_rgb(207, 54, 223), // #cf36df
+        Color::from_rgb(55, 181, 217), // #37b5d9
+        Color::from_rgb(62, 182, 218), // #3eb6da
+    ];
+    let isize = isize.into();
+    gradient_shader::linear(
+        (
+            Point::new(0.0, 0.0),
+            Point::new(isize.width as f32, isize.height as f32),
+        ),
+        GradientShaderColors::Colors(&colors),
+        Some(&[0.0_f32, 0.4, 0.6, 0.7, 0.8, 1.0] as &[scalar]),
+        TileMode::Clamp,
+        None,
+        None,
+    )
+}
+
+// 在渲染
+
+fn louvre(
+    images: Vec<InputImage>,
+    _: Vec<String>,
+    options: GrayStandard,
+) -> Result<Vec<u8>, Error> {
+    let binding = options.convolute.unwrap_or("一般".to_string());
+    let convolute = binding.as_str();
     let gain = options.gain.unwrap_or(2.0);
     let bias = options.bias.unwrap_or(0.0);
-    let limit = options.limit.unwrap_or(20) as u8;
+    let light_cut = 128.0;
+    let dark_cut = options.dark_cut.unwrap_or(118.0);
+    let mut diff = !options.no_diff.unwrap_or(false);
+    let kuma = !options.gray.unwrap_or(false);
+    let full_bg = !options.no_bg.unwrap_or(false);
     let func = |images: Vec<Image>| {
-        
-        #[rustfmt::skip]
         let luma_matrix = ColorMatrix::new(
-            0.299, 0.587, 0.114, 0.0, 0.0,
-            0.299, 0.587, 0.114, 0.0, 0.0,
-            0.299, 0.587, 0.114, 0.0, 0.0,
-            0.0,   0.0,   0.0,   1.0, 0.0
+            0.299, 0.587, 0.114, 0.0, 0.0, //
+            0.299, 0.587, 0.114, 0.0, 0.0, //
+            0.299, 0.587, 0.114, 0.0, 0.0, //
+            0.0, 0.0, 0.0, 1.0, 0.0, //
         );
-
-        let luma_filter = ColorFilter::luma();
-        let Some(blur_filter) = image_filters::blur((0.5, 0.5), TileMode::Clamp, None, None) else {
-            return Err(Error::MemeFeedback(
-                "blur filter create Failed!".to_string(),
-            ));
-        };
-        #[rustfmt::skip]
-        let out = -1.0;
-        let sobel_x: [f32; 9] = 
-        [
-            out, out, out, 
-            out, 8.0, out,
-            out, out, out
-        ];
-        let kernel_offset = IPoint::new(1, 1); 
-        // 通常卷积核偏移为其尺寸的一半减一
-
-        let Some(edge_filter) = image_filters::matrix_convolution(
-            (3,3),
-            &sobel_x,
-            gain,
-            bias,
-            kernel_offset,
-            TileMode::Clamp,
-            true, // convolve_alpha，是否对alpha通道进行卷积
-            None,
+        let mut img = images.first().unwrap().to_owned();
+        img = img.color_matrix(luma_matrix);
+        if options.denoise.unwrap_or(false) {
+            const NINE: f32 = 1.0 / 9.0;
+            img = img.image_filter(
+                image_filters::matrix_convolution(
+                    (3, 3),
+                    &[NINE; 9],
+                    1.0,
+                    0.0,
+                    (1, 1),
+                    TileMode::Clamp,
+                    false,
+                    None,
+                    None,
+                )
+                .unwrap(),
+            )
+        }
+        let img_size = img.dimensions();
+        let filter = if convolute == "线稿" {
+            diff = false;
             None
-        ) else {
-            return Err(Error::MemeFeedback(
-                "edge filter create Failed!".to_string(),
-            ));
+        } else {
+            let mut builder = match convolute {
+                "极细" => simple_convolute_average_builder(3),
+                "精细" => simple_convolute_average_builder(5),
+                "一般" => simple_convolute_average_builder(7),
+                "稍粗" => simple_convolute_average_builder(9),
+                "超粗" => simple_convolute_average_builder(11),
+                "极粗" => simple_convolute_average_builder(13),
+                "浮雕" => MatrixConvolutionBuilder::new(
+                    (3, 3),
+                    [
+                        1.0, 1.0, 1.0, //
+                        1.0, 1.0, -1.0, //
+                        -1.0, -1.0, -1.0, //
+                    ],
+                    (1, 1),
+                ),
+                "超浮雕" => MatrixConvolutionBuilder::new(
+                    (5, 5),
+                    [
+                        1.0, 1.0, 1.0, 1.0, 1.0, //
+                        1.0, 1.0, 1.0, 1.0, 1.0, //
+                        1.0, 1.0, 1.0, -1.0, -1.0, //
+                        -1.0, -1.0, -1.0, -1.0, -1.0, //
+                        -1.0, -1.0, -1.0, -1.0, -1.0, //
+                    ],
+                    (2, 2),
+                ),
+                _ => unreachable!(""),
+            };
+            builder.bias = bias;
+            builder.gain = gain;
+            let filter = builder.build();
+            if filter.is_none() {
+                return Err(Error::MemeFeedback(
+                    "edge filter create Failed!".to_string(),
+                ));
+            };
+            filter
         };
-        #[rustfmt::skip]
-        let matrix = ColorMatrix::new(
-            2.0, 0.0, 0.0, 0.0, 0.0, // R' = 2*R + 0*G + 0*B + 0*A + 0
-            0.0, 2.0, 0.0, 0.0, 0.0, // G' = 0*R + 2*G + 0*B + 0*A + 0
-            0.0, 0.0, 2.0, 0.0, 0.0, // B' = 0*R + 0*G + 2*B + 0*A + 0
-            0.0, 0.0, 0.0, 1.0, 0.0  // A' = 0*R + 0*G + 0*B + 1*A + 0
-        );
-
-        let img = images
-            .first()
-            .unwrap();
-        
-        let img = if options.resize.unwrap_or(false){
-            &img.resize_exact((500,500))
-        }else{img};
-
-        let img = img.image_filter(blur_filter);
-
-        let img = 
-        match gray_standard.as_str(){
-            "rec709"=>img.color_filter(luma_filter),
-            "rec601"=>img.color_matrix(luma_matrix),
-            "rec601_gray"=>img.color_matrix(luma_matrix).color_filter(luma_filter),
-            "rec601_noprocess"=>{return Ok(img.color_matrix(luma_matrix));},
-            _ => return Err(Error::MemeFeedback("not support gray".to_string()))
-        }
-        .image_filter(edge_filter);
-        if options.no_postprocess.unwrap_or(false){
-            return Ok(img);
-        }
-        let img = img.color_matrix(matrix);
-        let mut bg = new_surface(img.dimensions());
-        bg.canvas().clear(Color::WHITE);
-
-        let Some(data) = img.peek_pixels() else {
-            return Err(Error::MemeFeedback(
-                "cannot decode image!".to_string(),
-            ));
+        let mut img1 = img.clone();
+        if let Some(filter) = filter {
+            img1 = img.image_filter(filter)
         };
-
-        let width = img.width() as f32;
-        let height = img.height() as f32;
-        let canvas = bg.canvas();
-
-        for y in 0..img.height() {
-            for x in 0..img.width() {
-                let alpha = data.get_alpha_f((x,y));
-                let color = data.get_color((x,y));
-                if color.a()<limit{
-                    continue;
+        let img2 = img.to_owned();
+        let img1_data = img1.peek_pixels().unwrap();
+        let img2_data = img2.peek_pixels().unwrap();
+        let mut final_img = new_surface(img_size);
+        let canvas = final_img.canvas();
+        for x in 0..img.width() {
+            for y in 0..img.height() {
+                let rgb = img1_data.get_color((x, y));
+                let mut r = rgb.r();
+                let mut g = rgb.g();
+                let mut b = rgb.b();
+                let mut a = rgb.a();
+                if diff {
+                    r = 128 + img2_data.get_color((x, y)).r() - r;
+                    let scale: f32 = 255.0 / (255.0 - light_cut - dark_cut);
+                    r = ((r as f32 - dark_cut) * scale).clamp(0.0, 255.0) as u8;
+                    (g, b) = (r, r);
+                    a = 255 - r;
                 }
-                let gradient_factor = (x + y) as f32 / (width + height);
-
-                let hue = 0.05 + 0.65 * gradient_factor;
-                let hue = hue.clamp(0.0, 1.0);
-                let saturation: f32 = 0.9;
-                let brightness = 0.8 + 0.1 * (1.0 - gradient_factor);
-                let brightness = brightness.clamp(0.0, 1.0);
-                let alpha = (alpha*2.0).min(1.0);
-                let color = HSV::from((hue*360.0,saturation,brightness)).to_color((alpha*255.0) as u8);
-                canvas.draw_point((x,y), &new_paint(color));
+                canvas.draw_point((x, y), &new_paint(Color::from_argb(a, r, g, b)));
             }
-        };
-        Ok(bg.image_snapshot())
+        }
+        if kuma {
+            canvas.draw_irect(
+                IRect::from_size(img_size),
+                Paint::default()
+                    .set_shader(create_kiss_gradient(img_size).unwrap())
+                    .set_blend_mode(BlendMode::SrcIn),
+            );
+        }
+        if full_bg {
+            canvas.draw_irect(
+                IRect::from_size(img_size),
+                new_paint(Color::WHITE).set_blend_mode(BlendMode::DstOver),
+            );
+        }
+        Ok(final_img.image_snapshot())
     };
 
     make_png_or_gif(images, func)
--- src/options.rs
+++ src/options.rs
@@ -17,27 +17,39 @@ macro_rules! number_option {
 
 #[derive(MemeOptions)]
 pub(crate) struct GrayStandard {
-    /// 灰度图标准
-    #[option(short, long,long_aliases = ["灰度图标准"], default = "rec601_gray", choices = ["rec601", "rec709","rec601_noprocess","rec601_gray"])]
-    pub gray_standard: Option<String>,
-
     /// 缩放
     #[option(short, long,long_aliases = ["缩放"], default = false)]
     pub resize: Option<bool>,
 
     /// 不进行后处理
-    #[option(short, long,long_aliases = ["取消后处理"], default = false)]
-    pub no_postprocess: Option<bool>,
+    #[option(short, long,long_aliases = ["取消绘制背景"], default = false)]
+    pub no_bg: Option<bool>,
 
-    
-    #[option(short, long, minimum = 0, maximum = 255, default = 20)]
-    pub limit: Option<i32>,
+    /// 黑白
+    #[option(long,long_aliases = ["黑白"], default = false)]
+    pub gray: Option<bool>,
 
-    #[option(short, long, minimum = 0.0, maximum = 30.0, default = 5.0)]
+    /// 取消差异化
+    #[option(short, long,long_aliases = ["取消差异化"], default = false)]
+    pub no_diff: Option<bool>,
+
+    /// 卷积矩阵
+    #[option(short, long,long_aliases = ["卷积矩阵"], default = "一般",choices = ["精细","一般","稍粗","超粗","极粗","浮雕","线稿","极细","超浮雕"])]
+    pub convolute: Option<String>,
+
+    #[option(short, long, minimum = 0.0, maximum = 30.0, default = 1.0)]
     pub gain: Option<f32>,
-    
+
     #[option(short, long, minimum = -30.0, maximum = 30.0, default = 0.0)]
     pub bias: Option<f32>,
+
+    /// 线迹轻重
+    #[option(long,long_aliases = ["线迹轻重"], minimum = 80.0, maximum = 126.0, default = 118.0)]
+    pub dark_cut: Option<f32>,
+
+    /// 降噪
+    #[option(short, long,long_aliases = ["降噪"], default = false)]
+    pub denoise: Option<bool>,
 }
 
 pub(crate) use number_option;
